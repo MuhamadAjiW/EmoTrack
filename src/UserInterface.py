387e5ac8287816d95737cb1b94d0d2ff63d635ac
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-04-15 11:40:01 +0700

    refactor: foldering, style

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -7,6 +7,9 @@ from os import path
 class overlaySignals(QObject):
     close = pyqtSignal()
 
+class mainSignals(QObject):
+    close = pyqtSignal()
+
 class customOverlay(QWidget):
     def __init__(self, parent=None):
         super(customOverlay, self).__init__(parent)
@@ -65,41 +68,48 @@ class customOverlay(QWidget):
         self.signals.close.emit()
 
 
-class Ui_HomeWindow(QWidget):
-    def __init__(self, parent=None):
-        super(Ui_HomeWindow, self).__init__(parent)
-
+class Ui_HomeWindow(object):
     def setupUi(self, HomeWindow, abspath):
-        HomeWindow.setObjectName("HomeWindow")
-        HomeWindow.setEnabled(True)
+        HomeWindow.setObjectName("Home")
         HomeWindow.resize(1280, 786)
-        HomeWindow.setMinimumSize(QSize(1280, 786))
-        HomeWindow.setMaximumSize(QSize(1280, 786))
 
         self.centralwidget = QFrame(HomeWindow)
-        self.centralwidget.setMinimumSize(QSize(1280, 786))
-        self.centralwidget.setMaximumSize(QSize(1280, 786))
-        self.centralwidget.setObjectName("centralwidget")
-        
-        self.horizontalLayoutWidget = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QRect(-1, 0, 1281, 791))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.centralwidget.setObjectName("stackedwidget")
 
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName("horizontalLayout")
 
-        self.OpeningPanel = QFrame(self.horizontalLayoutWidget)
-        self.OpeningPanel.setStyleSheet('background-color: rgb(255,255,255)')
-        self.OpeningPanel.setFrameShape(QFrame.StyledPanel)
-        self.OpeningPanel.setLineWidth(6)
+        self.OpeningPanel = QFrame(self.centralwidget)
+        self.OpeningPanel.setStyleSheet("QPushButton {\n"
+"    background: rgb(240, 240, 240);\n"
+"    border: 2px solid rgb(130, 130, 130);\n"
+"    border-width: 2px;\n"
+"    border-radius: 0px;\n"
+"    color: black;\n"
+"}\n"
+"QPushButton:hover {\n"
+"    background: rgb(215, 215, 215);\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background: rgb(200, 200, 200);\n"
+"}")
         self.OpeningPanel.setObjectName("OpeningPanel")
 
         self.OpeningQuotes = QLabel(self.OpeningPanel)
         self.OpeningQuotes.setGeometry(QRect(30, 128, 240, 40))
 
+        self.NameLabel = QLabel(self.OpeningPanel)
+        self.NameLabel.setGeometry(QRect(30, 64, 240, 35))
+        font = QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(22)
+
+        self.NameLabel.setFont(font)
+        self.NameLabel.setObjectName("NameLabel")
+        
         font = QFont()
         font.setFamily("Helvetica")
         font.setPointSize(13)
@@ -107,38 +117,39 @@ class Ui_HomeWindow(QWidget):
         self.OpeningQuotes.setFont(font)
         self.OpeningQuotes.setObjectName("OpeningQuotes")
 
-        self.NameLabel = QLabel(self.OpeningPanel)
-        self.NameLabel.setGeometry(QRect(30, 64, 240, 35))
-
-        font = QFont()
-        font.setFamily("Helvetica")
-        font.setPointSize(22)
-
-        self.NameLabel.setFont(font)
-        self.NameLabel.setObjectName("NameLabel")
-
-        self.QuitButton = customTextButton("Keluar", self.OpeningPanel)
+        self.QuitButton = QPushButton(self.OpeningPanel)
         self.QuitButton.setGeometry(QRect(30, 650, 190, 40))
+        self.QuitButton.setFont(font)
+        self.QuitButton.setAutoFillBackground(False)
+        self.QuitButton.setStyleSheet("text-align: left;\n"
+                                      "padding: 5px;\n")
+        self.QuitButton.setCheckable(False)
+        self.QuitButton.setChecked(False)
+        self.QuitButton.setAutoDefault(False)
+        self.QuitButton.setDefault(False)
+        self.QuitButton.setFlat(False)
         self.QuitButton.setObjectName("QuitButton")
+        self.QuitButton.text
         self.QuitButton.clicked.connect(sys.exit)
 
         self.MoodButton = customSVGButton(path.join(abspath, "Resource/moodIcon.svg"), path.join(abspath, "Resource/moodIconH.svg"), self.OpeningPanel)
-        self.MoodButton.setGeometry(QRect(30, 590, 40, 40))
+        self.MoodButton.setGeometry(QRect(30, 600, 40, 40))
         self.MoodButton.setObjectName("MoodButton")
+        self.MoodButton
         #TODO: self.MoodButton.clicked.connect()
 
         self.JournalButton = customSVGButton(path.join(abspath, "Resource/journalIcon.svg"), path.join(abspath, "Resource/journalIconH.svg"), self.OpeningPanel)
-        self.JournalButton.setGeometry(QRect(80, 590, 40, 40))
+        self.JournalButton.setGeometry(QRect(80, 600, 40, 40))
         self.JournalButton.setObjectName("JournalButton")
         #TODO: self.MoodButton.clicked.connect()
 
         self.SleepButton = customSVGButton(path.join(abspath, "Resource/sleepIcon.svg"), path.join(abspath, "Resource/sleepIconH.svg"), self.OpeningPanel)
-        self.SleepButton.setGeometry(QRect(130, 590, 40, 40))
+        self.SleepButton.setGeometry(QRect(130, 600, 40, 40))
         self.SleepButton.setObjectName("SleepButton")
         #TODO: self.SleepButton.clicked.connect()
         
         self.QuotesButton = customSVGButton(path.join(abspath, "Resource/quoteIcon.svg"), path.join(abspath, "Resource/quoteIconH.svg"), self.OpeningPanel)
-        self.QuotesButton.setGeometry(QRect(180, 590, 40, 40))
+        self.QuotesButton.setGeometry(QRect(180, 600, 40, 40))
         self.QuotesButton.setObjectName("QuotesButton")
         #TODO: self.QuotesButton.clicked.connect()
 
@@ -149,11 +160,11 @@ class Ui_HomeWindow(QWidget):
 
         self.horizontalLayout.addWidget(self.OpeningPanel)
 
-        self.ImagePanel = QFrame(self.horizontalLayoutWidget)
+        self.ImagePanel = QFrame(self.centralwidget)
         self.ImagePanel.setFrameShape(QFrame.StyledPanel)
         self.ImagePanel.setLineWidth(6)
         self.ImagePanel.setObjectName("ImagePanel")
-        self.OpeningImage = customImage(QPixmap(path.join(abspath, "Resource/opening.png")), self.ImagePanel)
+        self.OpeningImage = customSVGImage(path.join(abspath, "Resource/opening.svg"), self.ImagePanel)
         self.OpeningImage.setGeometry(QRect(1, 1, 959, 790))
         self.OpeningImage.setObjectName("OpeningImage")
 
@@ -173,12 +184,13 @@ class Ui_HomeWindow(QWidget):
         self.OpeningQuotes.setText(_translate("HomeWindow", "Di dalam kesulitan pasti ada\n"
 "kemudahan"))
         self.NameLabel.setText(_translate("HomeWindow", "Hai Sayang"))
+        self.QuitButton.setText(_translate("Form", "Keluar"))
 
 
     def _onpopup(self):
         self.popup = customOverlay(self.centralwidget)
         self.popup.move(0, 0)
-        self.popup.resize(1280, 786)
+        self.popup.resize(HomeWindow.width(), HomeWindow.height())
         self.popup.signals.close.connect(self._closepopup)
         self.popup.show()
 
@@ -196,6 +208,7 @@ if __name__ == "__main__":
     HomeWindow = QMainWindow()
     ui = Ui_HomeWindow()
     ui.setupUi(HomeWindow, abspath)
+    HomeWindow.resizeEvent
 
     HomeWindow.show()
     sys.exit(app.exec_())
