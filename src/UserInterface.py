b2e91b615c1d75cb87dbd351c5a365ce59c417f9
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-04-19 18:18:21 +0700

    feat: window icon and title

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -12,6 +12,8 @@ from os import path
 class UserInterface(QMainWindow):
     def __init__(self, startView: UIWindow, views: dict, parent=None):
         super(UserInterface, self).__init__(parent)
+        self.setWindowIcon(QIcon(path.join(path.dirname(path.abspath(__file__)), "Resource/nameIcon.png")))
+        self.setWindowTitle("EmoTrack")
         self.setObjectName("UI Controller")
         self.setFixedSize(1280, 786)
 
